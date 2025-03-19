cc6e7392a4fb1f883ae4ee4b684549105047b480
Music_Bingo <krischun1@gmail.com>
2025-03-19 22:54:18 +0200

    sql update

--- phpchatapp_db.sql
+++ phpchatapp_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Време на генериране: 28 фев 2025 в 21:09
+-- Време на генериране: 19 март 2025 в 20:53
 -- Версия на сървъра: 9.1.0
 -- Версия на PHP: 8.3.14
 
@@ -34,47 +34,23 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `outgoing_msg_id` varchar(255) DEFAULT NULL,
   `msg` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`msg_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=63 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Схема на данните от таблица `messages`
 --
 
 INSERT INTO `messages` (`msg_id`, `incoming_msg_id`, `outgoing_msg_id`, `msg`) VALUES
-(1, '763849130', '802616344', 'hello'),
-(2, '802616344', '763849130', 'go'),
-(3, '802616344', '763849130', 'с'),
-(4, '802616344', '763849130', 'как е брат ми'),
-(5, '763849130', '802616344', 'бива'),
-(6, '763849130', '802616344', 'само така'),
-(7, '763849130', '802616344', 'има ме ли домашна'),
-(8, '802616344', '763849130', 'не'),
-(9, '802616344', '763849130', 'кккккк'),
-(10, '802616344', '763849130', 'пмцпям лмацс  поммммм'),
-(11, '1071410374', '763849130', 'ko '),
-(12, '1071410374', '763849130', 'sh ei'),
-(13, '763849130', '1071410374', 'ko be '),
-(14, '763849130', '1071410374', 'q ma ostai'),
-(15, '1071410374', '763849130', 'nqma '),
-(16, '763849130', '1071410374', 'mama ti'),
-(17, '1071410374', '763849130', 'ei bokluk'),
-(18, '763849130', '1071410374', 'dd'),
-(19, '802616344', '763849130', 'knwc'),
-(20, '253090620', '763849130', 'как сме '),
-(21, '763849130', '253090620', 'бива '),
-(22, '763849130', '253090620', 'цъкам spider man'),
-(23, '253090620', '763849130', 'оооо супер'),
-(24, '253090620', '763849130', 'готина игричка '),
-(25, '253090620', '763849130', 'за тез пари толкоз'),
-(26, '763849130', '253090620', 'да '),
-(27, '763849130', '253090620', 'ко да се прави '),
-(28, '253090620', '763849130', 'ами ай брат до после'),
-(29, '763849130', '253090620', 'ай'),
-(30, '253090620', '763849130', 'ko stawa'),
-(31, '763849130', '545570754', 'как е криска'),
-(32, '545570754', '763849130', 'бива братчето ми '),
-(33, '763849130', '673844406', 'дай ми пари'),
-(34, '673844406', '763849130', 'не');
+(62, '673844406', '1362537836', 'яй '),
+(61, '673844406', '1362537836', 'що бе '),
+(60, '1362537836', '673844406', 'я си гледай работата'),
+(59, '673844406', '1362537836', 'дай пари уе'),
+(58, '1362537836', '253090620', 'всичко е точно '),
+(55, '253090620', '1362537836', 'добре криска '),
+(56, '253090620', '1362537836', 'при теб как е '),
+(57, '1362537836', '253090620', 'бива '),
+(54, '1362537836', '253090620', 'как сме днес'),
+(53, '999582579', '1362537836', 'Ще идваш ли на уни');
 
 -- --------------------------------------------------------
 
@@ -93,19 +69,19 @@ CREATE TABLE IF NOT EXISTS `users` (
   `img` varchar(255) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL COMMENT 'Offline now, Online',
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Схема на данните от таблица `users`
 --
 
 INSERT INTO `users` (`user_id`, `unique_id`, `fname`, `lname`, `email`, `password`, `img`, `status`) VALUES
-(9, '253090620', 'Stancho', 'Naydenov', 'stancho@gmail.com', '$2y$10$8qn1hC2VGJ8riyWhVV4FSeNQLlfM3AOnRt3cM1x21DgNIMizXGOKi', '1740681428_1.png', 'online'),
-(10, '1071410374', 'Denkata', 'Kilima', 'denka@gmail.com', '$2y$10$xTqVMF31vKGrafT.NJMiYOzLvHHpVNE.6vknUbPPZSeiQvUawn1qW', '1740681492_1.png', 'online'),
-(11, '545570754', 'Slav', 'Dimitrov', 'slav@gmail.com', '$2y$10$kKNns7g1LUxjZdEEHtjzduYsrPj8Du1ke8ww/G7TeX3AJtxjsDMwi', '1740756298_1.png', 'offline now'),
-(12, '673844406', 'Димитър ', 'Димитров', 'batmitko@gmail.com', '$2y$10$CdGfdNGLvhhqJ0ZBdZxZiOV6kmZbQaot/.9rRVHtuITMeevmAkLQu', '1740761243_1.png', 'Online'),
-(8, '802616344', 'Gosho', 'Kostadinov', 'gosho@gmail.com', '$2y$10$X3p7CNM0OlOViK7F1DJFVOwvIhCgrOcCD538rDnaKvSI1j1BskC6W', '1740681384_1.png', 'online'),
-(7, '763849130', 'Kris', 'Dim', 'kdim@gmail.com', '$2y$10$mqGiRT/4TTm3l6uyW99I7OkUKLVdpz3Viuljd6OrIXbRk2FI7nzCO', '1740681291_1.png', 'offline now');
+(9, '253090620', 'Stancho', 'Naydenov', 'stancho@gmail.com', '$2y$10$8qn1hC2VGJ8riyWhVV4FSeNQLlfM3AOnRt3cM1x21DgNIMizXGOKi', '1740681428_1.png', 'offline now'),
+(10, '1071410374', 'Denkata', 'Kilima', 'denka@gmail.com', '$2y$10$xTqVMF31vKGrafT.NJMiYOzLvHHpVNE.6vknUbPPZSeiQvUawn1qW', '1741782829_Screenshot_2025-03-12-14-30-49-060_com.miui.gallery[1].jpg', 'offline now'),
+(11, '545570754', 'Tosho', 'Dimitrov', 'tosho@gmail.com', '$2y$10$kKNns7g1LUxjZdEEHtjzduYsrPj8Du1ke8ww/G7TeX3AJtxjsDMwi', 'cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA4L2pvYjEwMzQtZWxlbWVudC0wNi0zOTcucG5n.webp', 'offline now'),
+(12, '673844406', 'Димитър ', 'Димитров', 'batmitko@gmail.com', '$2y$10$CdGfdNGLvhhqJ0ZBdZxZiOV6kmZbQaot/.9rRVHtuITMeevmAkLQu', '488-4887957_facebook-teerasej-profile-ball-circle-circular-profile-picture.png', 'offline now'),
+(8, '802616344', 'Gosho', 'Kostadinov', 'gosho@gmail.com', '$2y$10$PgfvVph8krMeAp0m7F59B.I30ygtD5IeO2dNAsLkSfVlZNxxi/r32', 'cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTExL3Jhd3BpeGVsX29mZmljZV8zMl9waG90b19wb3J0cmFpdF9zbWlsaW5nX2luZGlhbl9tYW5faW5fc2hpcnRfYV84MWNjZTNiZS03ZTVjLTRkMDgtODcwYS03OTBlNjhlYWMwNTktbTNwanoycG8ucG5n.webp', 'offline now'),
+(14, '1362537836', 'KRIS', 'DIM', 'kdim@gmail.com', '$2y$10$dHrfIe6ShDJnFCJtErzTOup4Hkoa2xBE9dpjpBLuEiBA9WaA9q7ZO', 'images.jpg', 'offline now');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
